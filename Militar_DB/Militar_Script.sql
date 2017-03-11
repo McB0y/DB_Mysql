93871ec7a7965895306ca04f69667e3c134f01d6
McB0y <rafael.cisneros.gomez@gmail.com>
2017-03-11 03:46:15 -0600

    Reconfigurar para noSQL

--- Militar_DB/Militar_Script.sql
+++ Militar_DB/Militar_Script.sql
@@ -114,3 +114,5 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+--El modelo necesita crearse nuevamente para que pueda convertirse a noSQL
